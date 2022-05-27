f8f3cfb3d1ca0f14a180e4c20055e4d82b43231a
walze <wallacefares@gmail.com>
2022-05-27 12:32:15 -0300

    feat: added pixi

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import {createRoot} from 'react-dom/client'
 
 import {Application} from 'pixi.js'
 import { FC } from 'react'
 
-const pixi = new Application({
-  forceCanvas: true
-})
+
 
 const $root = document.getElementById('root')
 if (!$root) throw 'no root'
@@ -14,10 +12,21 @@ if (!$root) throw 'no root'
 const root = createRoot($root)
 
 const App: FC = () => {
+  const ref = useRef<HTMLCanvasElement | null>(null)
+
+  useEffect(() => {
+    if(!ref.current) return
+
+    const pixi = new Application({
+      view: ref.current,
+    })
+
+    console.log(1,pixi, ref.current)
+  }, [])
+
 
   return (<>
-    <canvas id='canvas' />
-    bruh3
+    <canvas id='canvas' ref={ref} />
   </>)
 }
 
